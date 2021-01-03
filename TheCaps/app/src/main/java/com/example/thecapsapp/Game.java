e123e448d06020cf29fb0ff32294331a3ab14127
gurparteet <gurparteetsinghpurbe@gmail.com>
2021-01-03 00:07:02 -0500

    TheCaps:Final Version

---
+++ TheCaps/app/src/main/java/com/example/thecapsapp/Game.java
@@ -0,0 +1,54 @@
+
+
+//Name- Gurparteet Singh Purbe
+//Student- 217604851
+
+
+
+
+package com.example.thecapsapp;
+import java.util.List;
+import java.util.Map;
+
+import ca.roumani.i2c.Country;
+import ca.roumani.i2c.CountryDB;
+
+public class Game {
+    private CountryDB db;
+    private String result;
+
+
+    public Game(){
+        this.db= new CountryDB();
+
+    }
+    public String qa(){
+        List<String> capitals=db.getCapitals();
+        int numberOfCapitals= capitals.size();
+        int index = (int)(numberOfCapitals  * Math.random()) ;
+        String c =capitals.get(index);
+
+        Map<String, Country> data=db.getData() ;
+        Country ref =data.get(c) ;
+
+        String countryName , capitalName ;
+        countryName=ref.getName();
+        capitalName=ref.getCapital();
+
+
+        if (Math.random() < 0.5){
+            String question= "What is the capital of  " + countryName+ " ?" ;
+             this.result = question + "\n" + capitalName ;
+        }
+        else {
+            String question= ref.getCapital() + " is capital of?" ;
+            this.result = capitalName +" is the capital of? \n"+ countryName ;
+
+        }
+        return result;
+    }
+
+
+
+
+}
